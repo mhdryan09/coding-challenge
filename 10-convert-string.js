dee1a0883533845a620f92c9b354a358dc3c4d69
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-03-15 10:44:01 +0700

    #day10 - convert a string to an array

---
+++ 10-convert-string.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// day 10/366
+
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// function stringToArray(string) {
+// 	return string.split(" ");
+// }
+
+// arrow function
+const stringToArray = string => string.split(" ");
